a09ef150a029e48b85c014979eb9d1b3561134db
jkrol1 <jk.jankrol@gmail.com>
2023-06-14 11:08:13 +0200

    Refactor LocalPathBuilderSpec test

--- src/test/scala/unit/LocalPathBuilderSpec.scala
+++ src/test/scala/unit/LocalPathBuilderSpec.scala
@@ -7,14 +7,14 @@ import org.scalatest.flatspec.AnyFlatSpec
 import com.scalawebscraping.core.LocalPathBuilder
 
 class LocalPathBuilderSpec extends AnyFlatSpec {
-  "A LocalPathBuilder" should "build raw scraped data path for local environment and specified scraper name and file extension" in {
+  "A LocalPathBuilder" should "build local environment path for raw scraped data, specified scraper name and file extension" in {
     val regex: Regex = """scraped_raw\/ScraperTest_\d+\.json""".r
     val builtPath = LocalPathBuilder.buildForScraped("ScraperTest", "json")
 
     assert(regex.matches(builtPath))
   }
 
-  it should "build parsed elements path for local environment and specified scraper name and file extension" in {
+  it should "build local environment path for parsed html, specified scraper name and file extension" in {
     val regex: Regex = """scraped_parsed\/ScraperTest_\d+\.json""".r
     val builtPath = LocalPathBuilder.buildForParsedElements("ScraperTest", "json")
 
